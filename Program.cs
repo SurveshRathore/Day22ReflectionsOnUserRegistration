ed0aac75e409d4915a91dd082e49adb6f70b0fb6
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-10-27 17:04:11 +0530

    master branch

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Day19UserRegistrationProblem;
+
+//UC1ValidateFirstName validateFirstName = new();
+//validateFirstName.CheckFirstName();
+
+//UC2ValidateLastName validateLastName = new();
+//validateLastName.CheckLastName();
+
+//UC3ValidateEmail email = new();
+//email.CheckEmail();
+
+//UC4ValidateMobile mobile = new();
+//mobile.CheckMobileNumber();
+
+//UC5PassMin8Char passChar = new();
+//passChar.CheckCharacterInPassword();
+
+UC6PassOneUpper passUpperChar = new();
+passUpperChar.CheckUpperCaseInPassword();
+/*
+UC7PassOneNumeric passNumeric = new();
+passNumeric.CheckNumericInPassword();
+
+UC8PassSpecialChar passSpecial = new();
+passSpecial.CheckSpecialCharInPassword();
+*/
+//UC9EmailSample emailValidate = new();
+//emailValidate.CheckEmail();
