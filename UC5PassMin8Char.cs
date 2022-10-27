9b45a4a642085e7f6bee845b1c2391cef97a69e0
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-10-27 17:00:04 +0530

    UC5 to validate the user password of min 8 char

---
+++ UC5PassMin8Char.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationProblem
+{
+    internal class UC5PassMin8Char
+    {
+        public void CheckCharacterInPassword()
+        {
+            //password regex
+            String nameRegex = "[A-Za-z0-9]{8}";
+            //here \\s for space
+            //^[a-zA-Z]{7}$ fix length
+            //^[a-zA-Z]{3,7}$ length range
+            Console.Write("Enter the password with minimum 8 characters: ");
+            String passChar = Console.ReadLine();
+            //compare the password regex with the user entered password
+            bool valid = Regex.IsMatch(passChar, nameRegex);
+            Console.WriteLine(valid);
+        }
+    }
+}
