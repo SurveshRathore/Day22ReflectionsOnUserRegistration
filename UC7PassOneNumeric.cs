0dc88ca2f16c2e5f0397b529dd3c8db3999087df
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-10-27 17:01:49 +0530

    UC7 to validate the user pass have a number

---
+++ UC7PassOneNumeric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationProblem
+{
+    internal class UC7PassOneNumeric
+    {
+        public void CheckNumericInPassword()
+        {
+            //one character in uppercase + one number password regex
+            String nameRegex = "(?=.*[A-Z])(?=.*[0-9]).{8}";
+            Console.Write("Enter the password with minimum 8 characters: ");
+            String passChar = Console.ReadLine();
+            //compare the password regex with the user entered password
+            bool valid = Regex.IsMatch(passChar, nameRegex);
+            Console.WriteLine(valid);
+        }
+    }
+}
