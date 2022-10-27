ab6628b68f811d2d27001336c3abe26631a694c3
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-10-27 17:00:53 +0530

    UC6 to validate the user pass have a upper char

---
+++ UC6PassOneUpper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationProblem
+{
+    internal class UC6PassOneUpper
+    {
+        public void CheckUpperCaseInPassword()
+        {
+            //one character in uppercase password regex
+            String nameRegex = "(?=.*[A-Z]).{8}";
+            Console.Write("Enter the password with minimum 8 characters: ");
+            String passChar = Console.ReadLine();
+            //compare the password regex with the user entered password
+            bool valid = Regex.IsMatch(passChar, nameRegex);
+            Console.WriteLine(valid);
+        }
+    }
+}
