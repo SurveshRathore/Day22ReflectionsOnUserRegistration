bfdea8d919ac595b0b5dbad3ab3928c7c59829d0
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-10-27 16:55:58 +0530

    UC2 to validate the user last name

---
+++ UC2ValidateLastName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationProblem
+{
+    internal class UC2ValidateLastName
+    {
+        public void CheckLastName()
+        {
+            //last name regex
+            String nameRegex = "^[A-Z][a-zA-Z]{2,}"; 
+            Console.Write("Enter the Last Name: ");
+            String LastName = Console.ReadLine();
+            //compare the last name regex with the user entered last name
+            bool valid = Regex.IsMatch(LastName, nameRegex);
+            Console.WriteLine(valid);
+        }
+    }
+}
